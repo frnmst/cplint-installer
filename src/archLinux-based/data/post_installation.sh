ef804266e6bb093a3a1158651ce6d291e249c40b
frnmst/Franco Masotti <franco.masotti@live.com>
2016-10-12 12:56:15 +0200

    Fixed systemd service and post installation script.

--- src/archLinux-based/data/post_installation.sh
+++ src/archLinux-based/data/post_installation.sh
@@ -27,10 +27,5 @@ getent group swish &>/dev/null || groupadd -r swish >/dev/null
 getent passwd swish &>/dev/null || useradd -r -g swish -s \
 /bin/false swish >/dev/null
 
-# Add the pid file.
-touch /run/swish-cplint.pid
-chown swish:swish /run/swish-cplint.pid
-
 # Create the dest pack directory.
 mkdir /usr/share/swish-cplint/pack
-
